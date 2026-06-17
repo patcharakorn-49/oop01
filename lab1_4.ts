fc5eb361e50d9f1c832bb922dc4b49e3167835b0
patcharakorn014 <patcharakorn49@gmail.com>
2026-06-17 16:55:45 +0700

    ส่ง lab1_4

---
+++ lab1_4.ts
@@ -0,0 +1,9 @@
+let Income:number = 50000;
+let Rate:number = 7
+let Tax:number = Income *0.07
+let Net:number = Income - Tax;
+
+console.log("Gross Income:%d ",Income);
+console.log("Tax Rate:%d %",Rate);
+console.log("Tax Paid:%d",Tax);
+console.log("Net Income after Tax:%d",Net);
